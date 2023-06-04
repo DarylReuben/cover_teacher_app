b7a48d4c435da273085f4b950abff4e6267c921e
DarylReuben <90577869+DarylReuben@users.noreply.github.com>
2023-06-04 22:23:11 +0200

    Update button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Deets from './Deets';
+
 import { Link } from 'react-router-dom';
 import Filters from './Filters';
+import { ArrowRight } from './Icons';
 
 
 function Card(props) {
@@ -13,13 +14,15 @@ function Card(props) {
       <Filters />
       {info.map((item, index) => (
         <div key={index} className=''>
+          <Link to="/Deets">
           <div className="bg-slate-200 m-1 rounded-2xl grid grid-rows-3 grid-flow-col gap-2">
             <div className="w-32 h-32 rounded-full bg-red-700 row-span-3">
               <img src={item.image} alt="" className="transition duration-300 ease-in-out hover:scale-110 rounded-full object-cover w-32 h-32" />
             </div>
             <div className="col-span-2 pl-8 text-lg font-bold"> {item.name} {item.surname} </div>
-            <div className="row-span-2 col-span-2 w-52"> {item.age}{item.sex}  <Link to="/Deets">deets</Link> </div>
+            <div className="row-span-2 col-span-2 w-52"> {item.age}{item.sex} </div>
           </div>
+          </Link>
         </div>
       ))}
     </div>
