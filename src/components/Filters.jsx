2e63b2fa280375e751932b9c351256a66eb42ebb
DarylReuben <90577869+DarylReuben@users.noreply.github.com>
2023-06-08 03:20:29 +0700

    filters update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -4,8 +4,13 @@ import {
   Button,
   Typography,
   IconButton,
+  Checkbox,
+  Card,
+  List,
+  ListItem,
+  ListItemPrefix,
 } from "@material-tailwind/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon, UserCircleIcon  } from "@heroicons/react/24/outline";
  
 export default function Filters() {
   const [openTop, setOpenTop] = React.useState(false);
@@ -17,7 +22,7 @@ export default function Filters() {
   return (
     <React.Fragment>
       <div className="flex flex-wrap gap-4 bg-slate-200 rounded-2xl">
-        <Button className="text-red-700"onClick={openDrawerTop}>Filters</Button>
+        <Button className=" text-primary-900 bg-sky-200"onClick={openDrawerTop}>Filters</Button>
 
       </div>
       <Drawer
@@ -28,7 +33,76 @@ export default function Filters() {
       >
         <div className="mb-6 flex items-center justify-between">
           <Typography variant="h5" color="blue-gray">
-            Drawer on Top
+
+          <Card className="w-full max-w-[24rem]">
+            <List className="flex-row">
+              <ListItem className="p-0">
+                <label htmlFor="horizontal-list-react" className="px-3 py-2 flex items-center w-full cursor-pointer">
+                  <ListItemPrefix className="mr-3">
+                    <Checkbox 
+                      id="horizontal-list-react" 
+                      ripple={false} 
+                      className="hover:before:opacity-0"
+                      containerProps={{
+                        className: "p-0"
+                      }}
+                    />
+                  </ListItemPrefix>
+                  <Typography color="blue-gray" className="font-medium">Male</Typography>
+                </label>
+              </ListItem>
+              <ListItem className="p-0">
+                <label htmlFor="horizontal-list-vue" className="px-3 py-2 flex items-center w-full cursor-pointer">
+                  <ListItemPrefix className="mr-3">
+                    <Checkbox 
+                      id="horizontal-list-vue" 
+                      ripple={false} 
+                      className="hover:before:opacity-0"
+                      containerProps={{
+                        className: "p-0"
+                      }}
+                    />
+                  </ListItemPrefix>
+                  <Typography color="blue-gray" className="font-medium">Female</Typography>
+                </label>
+              </ListItem>
+            </List>
+          </Card>
+          <Card className="w-full max-w-[24rem]">
+            <List className="flex-row">
+              <ListItem className="p-0">
+                <label htmlFor="horizontal-list-react" className="px-3 py-2 flex items-center w-full cursor-pointer">
+                  <ListItemPrefix className="mr-3">
+                    <Checkbox 
+                      id="horizontal-list-react" 
+                      ripple={false} 
+                      className="hover:before:opacity-0"
+                      containerProps={{
+                        className: "p-0"
+                      }}
+                    />
+                  </ListItemPrefix>
+                  <Typography color="blue-gray" className="font-medium">Native</Typography>
+                </label>
+              </ListItem>
+              <ListItem className="p-0">
+                <label htmlFor="horizontal-list-vue" className="px-3 py-2 flex items-center w-full cursor-pointer">
+                  <ListItemPrefix className="mr-3">
+                    <Checkbox 
+                      id="horizontal-list-vue" 
+                      ripple={false} 
+                      className="hover:before:opacity-0"
+                      containerProps={{
+                        className: "p-0"
+                      }}
+                    />
+                  </ListItemPrefix>
+                  <Typography color="blue-gray" className="font-medium">Non-native</Typography>
+                </label>
+              </ListItem>
+            </List>
+          </Card>
+
           </Typography>
           <IconButton variant="text" color="blue-gray" onClick={closeDrawerTop}>
             <XMarkIcon strokeWidth={2} className="h-5 w-5" />
