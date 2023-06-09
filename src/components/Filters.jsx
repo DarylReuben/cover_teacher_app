26728ed8caf97ba72fe93c2245085e06aaecc1b0
DarylReuben <90577869+DarylReuben@users.noreply.github.com>
2023-06-09 11:02:57 +0700

    filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import {
-  Drawer,
-  Button,
-  Typography,
-  IconButton,
-  Checkbox,
-  Card,
-  List,
-  ListItem,
-  ListItemPrefix,
-  Switch,
-  Chip,
-  Radio,
-} from "@material-tailwind/react";
-import { XMarkIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { Drawer, Button, IconButton, Card, Switch } from "@material-tailwind/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useSearchParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -21,33 +8,48 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function Filters() {
   const [openTop, setOpenTop] = React.useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [selectedDays, setSelectedDays] = React.useState([]);
   const [startDate, setStartDate] = React.useState(new Date());
 
   const handleSwitchChange = (option) => {
     const sexParam = option === searchParams.get("sex") ? "" : `?sex=${option}`;
-    const dateParam = `&date=${startDate.toISOString().split("T")[0]}`; // Format date as YYYY-MM-DD
-    setSearchParams(sexParam + dateParam);
+    const newDate = startDate.toISOString().split("T", 1)[0];
+    setStartDate(new Date(newDate));
+    setSearchParams(sexParam + "&date=" + newDate);
   };
 
-  const openDrawerTop = () => setOpenTop(true);
-  const closeDrawerTop = () => setOpenTop(false);
+  const handleDone = () => {
+    setOpenTop(false);
+  };
+
+  const openDrawerTop = () => {
+    setOpenTop(true);
+  };
+
+  const closeDrawerTop = () => {
+    setOpenTop(false);
+  };
 
   return (
-    <React.Fragment>
+    <>
       <div className="flex flex-wrap gap-4 bg-slate-200">
-        <Button className="text-primary-900 bg-sky-200" onClick={openDrawerTop}>
+        <Button color="lightBlue" onClick={openDrawerTop}>
           Filters
         </Button>
       </div>
-      <Drawer placement="top" open={openTop} onClose={closeDrawerTop} className="p-4">
-        <Card className="w-full w-400 flex-row justify-between pb-10px">
-          <Link className="" to=".">
-            Clear
-          </Link>
+      <Drawer placement="top" open={openTop} onClose={closeDrawerTop} className="p-4 w-[500px] object-center">
+        <h1 className="flex">
+          First select date, then select sex. Clear to reset filters
+          <IconButton className="flex justify-end" onClick={closeDrawerTop}>
+            <XMarkIcon strokeWidth={2} className="h-5 w-5" />
+          </IconButton>
+        </h1>
+        <Card className="w-32 h-8 mt-6 justify-center pl-5">
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} minDate={new Date()} />
+        </Card>
+        <Card className="w-[200px] flex-row justify-between pb-10px mb-10">
           <Switch
             id="male"
-            label="male"
+            label="Male"
             checked={searchParams.get("sex") === "male"}
             onChange={() => handleSwitchChange("male")}
           />
@@ -58,14 +60,15 @@ export default function Filters() {
             onChange={() => handleSwitchChange("female")}
           />
         </Card>
-        <Card className="w-32 h-16 mt-6 bg-blue-gray-300">
-          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} minDate={new Date()} />
-          <Button onClick={() => handleSwitchChange(searchParams.get("sex"))}>Submit</Button>
-        </Card>
-        <IconButton className="flex justify-end" variant="text" color="blue-gray" onClick={closeDrawerTop}>
-          <XMarkIcon strokeWidth={2} className="h-5 w-5" />
-        </IconButton>
+        <div className="flex flex-row mt-[10px]">
+          <Link className="" to=".">
+            Clear
+          </Link>
+          <Button onClick={handleDone} color="lightBlue" buttonType="filled" size="sm">
+            Done
+          </Button>
+        </div>
       </Drawer>
-    </React.Fragment>
+    </>
   );
 }
